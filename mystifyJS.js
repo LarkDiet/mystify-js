323f7458d245c952de123a316e53dde6cf252bf5
Lark Henriquez <nightinglarkdiet@gmail.com>
2021-04-09 13:50:55 -0400

    removed old comments/spaghetti

--- mystifyJS.js
+++ mystifyJS.js
@@ -69,8 +69,6 @@ class Poly {
   
 }
 
-
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 255);
@@ -87,79 +85,3 @@ function draw() {
   poly2.update();
   isInit = false;
 }
-
-
-/*function initVectors() {
-  let v1 = createVector(width/4, height/4);
-  let v2 = createVector(width*0.75, height/4);
-  let v3 = createVector(width*0.75, height*0.75);
-  let v4 = createVector(width/4, height*0.75);
-  vectors = [v1, v2, v3, v4];
-  
-  for (i = 0; i < 4; i++) {
-    poly2.push(new p5.Vector(random(1, width), random(1, height)));
-  }
-}
-
-
-function update() {
-  if (isInit) {
-    for (i = 0; i < vectors.length; i++) {
-      targets.push(p5.Vector.random2D().mult(speed));
-    }
-    
-    for (i = 0; i < poly2.length; i++) {
-      targets2.push(p5.Vector.random2D().mult(speed));
-    }
-    isInit = false;
-  }
-  for (i = 0; i < vectors.length; i++) {
-    vectors[i] = vectors[i].add(targets[i]);
-    if (vectors[i].x < 0 || vectors[i].x > width) {
-      targets[i].x *= -1;
-    }
-    if (vectors[i].y < 0 || vectors[i].y > height) {
-      targets[i].y *= -1;
-    }
-  }
-  
-  for (i = 0; i < poly2.length; i++) {
-    poly2[i] = poly2[i].add(targets2[i]);
-    if (poly2[i].x < 0 || poly2[i].x > width) {
-      targets2[i].x *= -1;
-    }
-    if (poly2[i].y < 0 || poly2[i].y > height) {
-      targets2[i].y *= -1;
-    }
-  }
-  
-  colorPhase();
-}
-
-function render() {
-  for (i = 0; i < vectors.length; i++) {
-    if (i == vectors.length - 1) {
-      line(vectors[i].x, vectors[i].y, vectors[0].x, vectors[0].y);
-    } else {
-      line(vectors[i].x, vectors[i].y, vectors[i+1].x, vectors[i+1].y);
-    }
-  }
-  
-  for (i = 0; i < poly2.length; i++) {
-    if (i == poly2.length - 1) {
-      line(poly2[i].x, poly2[i].y, poly2[0].x, poly2[0].y);
-    } else {
-      line(poly2[i].x, poly2[i].y, poly2[i+1].x, poly2[i+1].y);
-    }
-  }
-}
-
-function colorPhase() {
-  let h = hue(polyColor);
-  if (h == 255) {
-    h = 0;
-  } else {
-    h += 0.1;
-  }
-  polyColor = color(h, 255, 255);
-} */
